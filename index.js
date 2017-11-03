09ae3195e7fc9cf502f55f897dae9bc4a18a823a
Ian Pavlovic <ian_pavlovic@msn.com>
2017-11-03 15:02:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const app = "I don't do much."
 function destructivelyAppendKitten(name){
   kittens.push(name)
+  return kittens
 }
 function destructivelyPrependKitten(name){
   kittens.unshift(name)
+  return kittens
 }
-function destructivelyRemoveLastKitten(name){
-  kittens.pop(name)
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
 }
-function destructivelyRemoveFirstKitten(name){
-  kittens.shift(name)
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
 }
 function appendKitten(name){
-  const moreCats = [...kittens, "Broom"]
-  return moreCats
+  return [...kittens, name]
 }
 function prependKitten(name){
-  const moreCats2 = ["Arnold", ...kittens]
-  return moreCats2
+  return [name, ...kittens]
 }
 function removeLastKitten(){
-  var cats = kittens.slice(0, kittens.length - 1)
-  return cats
+  return kittens.slice(0, kittens.length - 1)
 }
 function removeFirstKitten(){
-  var cat2 = kittens.slice(-2)
-  return cat2
+  return kittens.slice(1)
 }
